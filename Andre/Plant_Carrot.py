1abfe0be8dcedb7a0d41d9640d06e29bafc9fc91
Andre <andre.b.herzog@gmail.com>
2026-04-19 13:12:17 +0200

    Andre initial commit

---
+++ Andre/Plant_Carrot.py
@@ -0,0 +1,10 @@
+from __builtins__ import *
+
+def plant_carrot():
+	for i in range(get_world_size()):
+		if can_harvest():
+			harvest()
+		if get_ground_type() != Grounds.Soil:
+				till()
+		plant(Entities.Carrot)
+		move(North)
